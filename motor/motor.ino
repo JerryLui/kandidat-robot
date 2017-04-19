b3bd7911a05e97e73bfa8a1f7db4bc9e19aac355
JerryLui <jerry.liu@hotmail.se>
2017-04-19 14:50:02 +0200

    Added acceleration & deacceleration methods.

--- motor/motor.ino
+++ motor/motor.ino
@@ -11,8 +11,12 @@
 #define rStep 6
 #define rOutput 2
 
-#define motorDelayLowerBound 6000		// Delaytime for motor
-#define motorDelayUpperBound 2000
+#define motorDelayUpperBound 6000		// Delaytime for motor
+#define motorDelayLowerBound 2000
+#define delayIncrement 40
+#define accelerationLength 100			// The number of steps required for accelration/deacceleration
+
+unsigned int testVar;
 
 // Motor Directions
 enum Direction {
@@ -23,20 +27,68 @@ enum Direction {
 void setup() {
 	pinMode(lOutput, OUTPUT);pinMode(lStep, OUTPUT);pinMode(lDirection, OUTPUT);
 	pinMode(rOutput, OUTPUT);pinMode(rStep, OUTPUT);pinMode(rDirection, OUTPUT);
-	digitalWrite(lOutput, LOW);digitalWrite(lDirection, LOW);
-	digitalWrite(rOutput, LOW);digitalWrite(rDirection, HIGH);
+	digitalWrite(lOutput, LOW);digitalWrite(lDirection, HIGH);
+	digitalWrite(rOutput, LOW);digitalWrite(rDirection, LOW);
+	testVar = 1;
 }
 
 // Main Loop
 void loop() {
-	step(motorDelayLowerBound);
+	if (testVar == 1) {
+		walk(STRAIGHT, 600);
+		testVar = 2;
+	}
 }
 
 // Motor Functions
+
+// Configures motor for the given direction dir walks for given steps
 void walk(Direction dir, int steps) {
-	int currentDelay = motorDelayUpperBound;
 	direction(dir);
+	deaccelerate(accelerationLength, accelerate(steps));
+}
+
+// Runs motor for given amount of steps
+void runMotor(int steps, int currentDelay) {
 	for (int i = 0; i < steps; i++) {
+		step(currentDelay);
+	}
+}
+
+// Accelerates in given number of steps, returns the current motor delay
+int accelerate(int steps) {
+	int currentDelay = motorDelayUpperBound;
+	for (int i = 0; i < steps; i++) {
+		step(currentDelay);
+		currentDelay = decrementDelay(currentDelay);
+	}
+	return currentDelay;
+}
+
+// Deaccelerates in given number of steps, returns the current motor delay
+int deaccelerate(int steps, int currentDelay) {
+	for (int i = 0; i < steps; i++) {
+		step(currentDelay);
+		currentDelay = incrementDelay(currentDelay);
+	}
+	return currentDelay;
+}
+
+// Increments motor delay time by delayIncrement
+int incrementDelay(int delayTime) {
+	if (delayTime < motorDelayUpperBound) {
+		return delayTime + delayIncrement;
+	} else {
+		return delayTime;
+	}
+}
+
+// Decrements motor delay time by delayIncrement
+int decrementDelay(int delayTime) {
+	if (delayTime > motorDelayLowerBound) {
+		return delayTime - delayIncrement;
+	} else {
+		return delayTime;
 	}
 }
 
@@ -60,8 +112,8 @@ void direction(Direction dir) {
 			digitalWrite(rDirection, HIGH);
 			break;
 		case STRAIGHT:
-			digitalWrite(lDirection, LOW);
-			digitalWrite(rDirection, HIGH);
+			digitalWrite(lDirection, HIGH);
+			digitalWrite(rDirection, LOW);
 			break;
 	}
 }
